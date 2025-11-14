9882359be299115f40c89ae9195bcfd3603afaf0
Gabriel_Leal <glealleone@gmail.com>
2025-11-14 15:34:09 -0300

    HashTable: put(), get() e remove() adcionados

--- HashTable.java
+++ HashTable.java
@@ -35,7 +35,7 @@ public class HashTable<K, V> {
         this.table = (LinkedList<Entry<K, V>>[]) new LinkedList[capacity];
 
 
-        /* Esta lógica vem do exemplo de implementação do professor:
+        /* Essa lógica vem do exemplo de implementação do professor:
          * Precisamos inicializar CADA posição do array com uma lista vazia.
          * Sem isso teremos NullPointerException ao tentar adicionar o primeiro item em um bucket.
          */
@@ -88,18 +88,50 @@ public class HashTable<K, V> {
         return hash;
     }
     
-    public void put(K key, V value) {
-        //
+    // Inserir a entry ou atualizar o valor
+    public void put(K key, V value) { 
+        int index = hash(key);
+        LinkedList<Entry<K, V>> bucket = table[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                // CASO 1: CHAVE ENCONTRADA -> ATUALIZAÇÃO DO VALOR
+                entry.value = value;
+                return;
+            }
+        }
+
+        // CASO 2: CHAVE NÃO ENCONTRADA -> INSERÇÃO
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        bucket.add(newEntry);
+        size++;
     }
     
     public V get(K key) {
-        //
+        int index = hash(key);
+        LinkedList<Entry<K, V>> bucket = table[index];
+
+        for (Entry<K, V> entry : bucket) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+
         return null;
     }
     
-    public V remove(K key) {
-        //
-        return null;
+    public boolean remove(K key) {
+        int index = hash(key);
+        LinkedList<Entry<K, V>> bucket = table[index];
+
+        // Remova o entry SE a chave dele for igual a chave passada
+        boolean removed = bucket.removeIf(entry -> entry.key.equals(key));
+
+        if (removed) {
+            size--;
+        }
+        
+        return removed;
     }
 
     public int getSize() {
